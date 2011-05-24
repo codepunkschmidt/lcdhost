d799293a7083769ff0d4daddc339fc8eda079ecb
linkdata.se <linkdata.se@localhost>
2011-05-24 17:56:18 +0200

    win32 fixes for api 5

--- LH_LgLcdMan/EventLgLcdButton.h
+++ LH_LgLcdMan/EventLgLcdButton.h
@@ -1,16 +1,16 @@
 #ifndef EVENTLGLCDBUTTON_H
 #define EVENTLGLCDBUTTON_H
 
-#include "EventBase.h"
+#include "QEvent"
 
-class EventLgLcdButton : public EventBase
+class EventLgLcdButton : public QEvent
 {
 public:
     bool bw;
     unsigned long buttons;
 
-    EventLgLcdButton( bool b, unsigned long ul ) : EventBase( type() ), bw(b), buttons(ul) {}
-    static QEvent::Type type() { static QEvent::Type typeVal = static_cast<QEvent::Type>(registerEventType(__FILE__)); return typeVal; }
+    EventLgLcdButton( bool b, unsigned long ul ) : QEvent( type() ), bw(b), buttons(ul) {}
+    static QEvent::Type type() { static QEvent::Type typeVal = static_cast<QEvent::Type>(registerEventType()); return typeVal; }
 };
 
 

e00480329d75da505b5fa6322b6be03249ed7bcb
linkdata.se <linkdata.se@localhost>
2011-07-10 21:56:11 +0200

    Backlight

--- LH_LgBacklight/LH_LgBacklight.cpp
+++ LH_LgBacklight/LH_LgBacklight.cpp
@@ -34,11 +34,6 @@
 
 #include <QFile>
 #include <QDebug>
-
-#ifdef Q_WS_WIN
-#include <windows.h>
-#endif
-
 #include "LH_LgBacklight.h"
 
 LH_PLUGIN(LH_LgBacklight)
@@ -66,8 +61,18 @@ char __lcdhostplugin_xml[] =
 const char *LH_LgBacklight::userInit()
 {
     if( const char *err = LH_QtPlugin::userInit() ) return err;
-    rescanbutton_ = new LH_Qt_QString( this, "Rescan","Rescan",LH_FLAG_NOSAVE|LH_FLAG_NOSOURCE,lh_type_string_button );
+    devselect_ = new LH_Qt_QStringList( this, "Device", QStringList("Default"), LH_FLAG_NOSAVE );
+    connect( devselect_, SIGNAL(changed()), this, SLOT(changeDev()) );
+    devcolor_ = new LH_Qt_QColor( this, "Color", Qt::white, LH_FLAG_NOSAVE );
+    connect( devcolor_, SIGNAL(changed()), this, SLOT(changeColor()) );
+    devisdefault_ = new LH_Qt_bool( this, "Default",false,LH_FLAG_HIDDEN|LH_FLAG_NOSAVE);
+    connect( devisdefault_, SIGNAL(changed()), this, SLOT(defaultCheckbox()) );
+    rescanbutton_ = new LH_Qt_QString( this, "Rescan","Rescan",
+                                       LH_FLAG_LAST|LH_FLAG_NOSAVE|LH_FLAG_NOSOURCE, lh_type_string_button );
+    defaultdev_ = new LH_Qt_QString( this, "DefaultDevice", QString(), LH_FLAG_HIDDEN );
+    connect( defaultdev_, SIGNAL(changed()), this, SLOT(changeDev()) );
     connect( rescanbutton_, SIGNAL(changed()), this, SLOT(scan()) );
+    changeDev();
     scan();
     return NULL;
 }
@@ -86,10 +91,12 @@ void LH_LgBacklight::scan()
                 switch( hdi->product_id )
                 {
                 case 0xC222: /* G15 */
+                case 0xC225: /* G11 */
                 case 0xC227: /* G15v2 */
                 case 0xC21C: /* G13 */
                 case 0xC22D: /* G510 without audio */
                 case 0xC22E: /* G510 with audio */
+                case 0xC229: /* G19 */
                     {
                         bool found = false;
                         foreach( LgBacklightDevice *d, devs_ )
@@ -101,7 +108,7 @@ void LH_LgBacklight::scan()
                                 break;
                             }
                         }
-                        if( !found ) new LgBacklightDevice(hdi,this);
+                        if( !found ) devs_.append( new LgBacklightDevice(hdi,this) );
                     }
                     break;
                 case 0x0A07: /* Z10 */
@@ -113,6 +120,8 @@ void LH_LgBacklight::scan()
         hid_free_enumeration( hdi_head );
     }
 
+    devselect_->list().clear();
+    devselect_->list().append("Default");
     foreach( LgBacklightDevice *d, devs_ )
     {
         if( d->removal() )
@@ -120,7 +129,50 @@ void LH_LgBacklight::scan()
             devs_.removeAll(d);
             delete d;
         }
+        else
+        {
+            devselect_->list().append( d->name() );
+        }
     }
+    devselect_->refreshList();
 
     return;
+}
+
+void LH_LgBacklight::changeDev()
+{
+    QColor usecolor;
+    devisdefault_->setHidden( devselect_->value() < 1 );
+    devisdefault_->setValue( devselect_->valueText() == defaultdev_->value() );
+    foreach( LgBacklightDevice *d, devs_ )
+    {
+        if( d->name() == devselect_->valueText() ||
+            (d->name() == defaultdev_->value() && !usecolor.isValid()) )
+            usecolor = d->color();
+    }
+    devcolor_->setValue( usecolor );
+}
+
+void LH_LgBacklight::changeColor()
+{
+    QString lookfor;
+    if( devselect_->value() < 1 ) lookfor = defaultdev_->value();
+    else lookfor = devselect_->valueText();
+
+    foreach( LgBacklightDevice *d, devs_ )
+    {
+        if( d->name() == lookfor )
+        {
+            d->setColor( devcolor_->value() );
+            break;
+        }
+    }
+}
+
+void LH_LgBacklight::defaultCheckbox()
+{
+    if( devisdefault_->value() )
+        defaultdev_->setValue( devselect_->valueText() );
+    else
+        defaultdev_->setValue( QString() );
 }

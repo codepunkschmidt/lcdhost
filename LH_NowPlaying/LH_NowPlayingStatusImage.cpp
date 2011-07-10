fc8fe776f8f611713beaf946c8d1dc1718fca61d
Andy Bridges <andy@bridgesuk.com>
2011-07-10 19:27:33 +0100

    LH_NowPlaying: Improved status image & fixed bug in text

--- LH_NowPlaying/LH_NowPlayingStatusImage.cpp
+++ LH_NowPlaying/LH_NowPlayingStatusImage.cpp
@@ -43,14 +43,53 @@ const char *LH_NowPlayingStatusImage::userInit()
     if( const char *err = LH_QImage::userInit() ) return err;
 
     setup_image_file_->setFlags( LH_FLAG_AUTORENDER | LH_FLAG_READONLY | LH_FLAG_NOSAVE );
-    setup_image_file_->setHelp("To set image shown, enable conditional formatting and create rules based on the Playing State.");
+    setup_image_file_->setHelp("To set image shown, enable conditional formatting and create rules based on the selected item.");
 
-    connect( currentTrack, SIGNAL(changed()), this, SLOT(refresh_status()) );
+    connect( currentTrack, SIGNAL(changed()), this, SLOT(refresh_value()) );
 
-    setup_playing_state_ = new LH_Qt_QString(this, "Playing State", "", LH_FLAG_NOSOURCE | LH_FLAG_NOSINK | LH_FLAG_NOSAVE | LH_FLAG_READONLY);
+    setup_item_ = new LH_Qt_QStringList(this, "Item",
+                                        QStringList()
+                                        <<"Track Title"
+                                        <<"Artist"
+                                        <<"Album"
+                                        <<"Duration"
+                                        <<"Time Played"
+                                        <<"Time Remaining"
+                                        <<"Status"
+                                        <<"Player"
+                                        <<"Custom"
+                                        <<"File Name",
+                                        LH_FLAG_FIRST
+                                        );
+    setup_item_->setValue(6);
+    setup_item_->setHelp("The item to base your rules on.<br/><br/>Note that not all players provide all data items. If you user such a player you may find a custom string a better choice.");
+    connect(setup_item_, SIGNAL(changed()), this, SLOT(setup_item_changed()));
+
+    setup_custom_ = new LH_Qt_QString(this, "^Custom", "{artist}{artist?: \"}{title}{artist?:\"} {status?[}{status}{status?]}", LH_FLAG_FIRST | LH_FLAG_HIDDEN);
+    setup_custom_->setHelp("Enter a template string.<br/><br/>"
+                           "Templates: <br/><br/>"
+                           "<table>"
+                           "<tr><td>Track Title:</td><td>{title}</td></tr>"
+                           "<tr><td>Artist:</td><td>{artist}</td></tr>"
+                           "<tr><td>Album:</td><td>{album}</td></tr>"
+                           "<tr><td>Duration:</td><td>{duration}</td></tr>"
+                           "<tr><td>Time Played</td><td>{played}</td></tr>"
+                           "<tr><td>Time Remaining</td><td>{remaining}</td></tr>"
+                           "<tr><td>Status</td><td>{status}</td></tr>"
+                           "<tr><td>Player</td><td>{player}</td></tr>"
+                           "</table><br/><br/>"
+                           "Additionally you can include text conditionally using {<token>?<text>} , <br/>"
+                           "e.g.: {album?Album: \"}{album}{album?\"} <br/>"
+                           "This displays nothing when no album name is provided; when an album name is available however it will show: <br/>"
+                           "Album: \"<album name>\"<br/>"
+                           );
+    connect(setup_custom_, SIGNAL(changed()), this, SLOT(refresh_value()));
+
+
+    setup_value_ = new LH_Qt_QString(this, "Select Item Value", "", LH_FLAG_NOSOURCE | LH_FLAG_NOSINK | LH_FLAG_NOSAVE | LH_FLAG_READONLY | LH_FLAG_FIRST | LH_FLAG_HIDETITLE);
 
     add_cf_target(setup_image_file_);
-    add_cf_source(setup_playing_state_);
+    add_cf_source(setup_value_);
 
     return 0;
 }
@@ -70,11 +109,52 @@ lh_class *LH_NowPlayingStatusImage::classInfo()
     return &classInfo;
 }
 
-void LH_NowPlayingStatusImage::refresh_status()
+void LH_NowPlayingStatusImage::setup_item_changed()
 {
-    QString status_text = "";
+    setup_custom_->setFlag(LH_FLAG_HIDDEN, setup_item_->value()!=8);
+    refresh_value();
+}
+
+void LH_NowPlayingStatusImage::refresh_value()
+{
+    QString template_value = "";
     if (currentTrack->playerFound())
     {
+        switch(setup_item_->value())
+        {
+        case 0:     //Track Title
+            template_value = "{title}";
+            break;
+        case 1:     //Artist
+            template_value = "{artist}";
+            break;
+        case 2:     //Album
+            template_value = "{album}";
+            break;
+        case 3:     //Duration
+            template_value = "{duration}";
+            break;
+        case 4:     //Time Played
+            template_value = "{played}";
+            break;
+        case 5:     //Time Remaining
+            template_value = "{remaining}";
+            break;
+        case 6:     //Status
+            template_value = "{status}";
+            break;
+        case 7:     //Player
+            template_value = "{player}";
+            break;
+        case 9:     //File
+            template_value = "{file}";
+            break;
+        //case 8:     //Custom
+        default:
+            template_value = setup_custom_->value();
+        }
+
+        QString status_text = "";
         switch(currentTrack->info().status)
         {
         case PLAYER_STATUS_CLOSED:
@@ -90,6 +170,33 @@ void LH_NowPlayingStatusImage::refresh_status()
             status_text = "Playing";
             break;
         }
+
+        replace_token(template_value,"title",currentTrack->info().track);
+        replace_token(template_value,"artist",currentTrack->info().artist);
+        replace_token(template_value,"album",currentTrack->info().album);
+        replace_token(template_value,"duration",currentTrack->info().totalSecs,currentTrack->info().totalSecs);
+        replace_token(template_value,"played",currentTrack->info().currentSecs,currentTrack->info().totalSecs);
+        replace_token(template_value,"remaining",currentTrack->info().totalSecs - currentTrack->info().currentSecs,currentTrack->info().totalSecs);
+        replace_token(template_value,"status",status_text);
+        replace_token(template_value,"player",currentTrack->info().player);
+        replace_token(template_value,"file",currentTrack->info().file);
     }
-    setup_playing_state_->setValue( status_text );
+    setup_value_->setValue( template_value );
+}
+
+void LH_NowPlayingStatusImage::replace_token(QString &str, QString token, QString val)
+{
+    QRegExp re1(QString("\\{%1\\}").arg(token));
+    str = str.replace(re1, val);
+    QRegExp re2(QString("\\{%1\\?([^}]*)\\}").arg(token));
+    if(val.trimmed()!="")
+        str = str.replace(re2, "\\1");
+    else
+        str = str.replace(re2, "");
+}
+
+void LH_NowPlayingStatusImage::replace_token(QString &str, QString token, int seconds, int totalSeconds)
+{
+    QTime t = QTime(0,0,0).addSecs(seconds);
+    replace_token(str,token,(totalSeconds==0? "" : t.toString((t.hour()>0? "hh:mm:ss" : "mm:ss"))));
 }

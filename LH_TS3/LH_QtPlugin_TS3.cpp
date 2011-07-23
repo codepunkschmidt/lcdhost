43c926c7da037317ff94deec98e54ae01c5ee347
Andy Bridges <andy@bridgesuk.com>
2011-07-23 18:08:37 +0100

    Working TS3 code

--- LH_TS3/LH_QtPlugin_TS3.cpp
+++ LH_TS3/LH_QtPlugin_TS3.cpp
@@ -51,14 +51,16 @@ const char *LH_QtPlugin_TS3::userInit()
 {
     if( const char *err = LH_QtPlugin::userInit() ) return err;
 
-    server_action_ = sa_none;
-
-    pollTimer_.start();
+    server_action_ = sa_disconnected;
+    tryConnectTimer_.start();
 
     socket_ = new QTcpSocket(this);
 
+    setup_status = new LH_Qt_QString(this,tr("Connection Status"),QString(),LH_FLAG_FIRST | LH_FLAG_NOSAVE | LH_FLAG_NOSINK | LH_FLAG_NOSOURCE | LH_FLAG_HIDETITLE, lh_type_string_htmlhelp );
+    updateStatus(false);
+
     setup_speakers = new LH_Qt_QString(this, "Speakers", "", LH_FLAG_READONLY | LH_FLAG_NOSAVE | LH_FLAG_NOSINK);
-    setup_speakers->setLink("@/monitoring/3rdParty/TS3/Speaking");
+    setup_speakers->setLink("@/Monitoring/3rdParty/TeamSpeak3/Speaking");
 
     connect(socket_, SIGNAL(connected()), this, SLOT(TS3Connected()));
     connect(socket_, SIGNAL(disconnected()), this, SLOT(TS3Disconnected()));
@@ -69,65 +71,140 @@ const char *LH_QtPlugin_TS3::userInit()
     return 0;
 }
 
+int LH_QtPlugin_TS3::notify( int code, void *param )
+{
+    if( code & LH_NOTE_SECOND && server_action_ == sa_disconnected  && tryConnectTimer_.elapsed() >= 5000)
+        openConnection();
+    return LH_NOTE_SECOND;
+}
+
 void LH_QtPlugin_TS3::openConnection()
 {
     switch(socket_->state())
     {
     case QAbstractSocket::UnconnectedState:
-        qDebug() << "LH_TS3: Attempting to connect.";
+        //qDebug() << "LH_TS3: Attempting to connect.";
+        server_action_ = sa_connecting;
         socket_->connectToHost("127.0.0.1", 25639);
         break;
     case QAbstractSocket::ConnectedState:
-        qWarning() << "LH_TS3: Cannot connect - already connected.";
+        //qWarning() << "LH_TS3: Cannot connect - already connected.";
         server_action_ = sa_reconnect;
         socket_->disconnectFromHost();
         break;
     case QAbstractSocket::ClosingState:
-        qWarning() << "LH_TS3: Cannot connect - still closing previous connection.";
+        //qWarning() << "LH_TS3: Cannot connect - still closing previous connection.";
         server_action_ = sa_reconnect;
         break;
     default:
-        qWarning() << "LH_TS3: Cannot connect - previous connection attempt was in progress - aborting.";
+        //qWarning() << "LH_TS3: Cannot connect - previous connection attempt was in progress - aborting.";
         server_action_ = sa_reconnect;
         socket_->abort();
     }
 }
 
 void LH_QtPlugin_TS3::TS3Connected() {
-    qDebug() << "LH_TS3: Connected";
-    server_action_ = sa_channellist_pending;
-    sendMessage("channellist");
+    //qDebug() << "LH_TS3: Connected";
+    updateStatus(true);
+
+    server_action_ = sa_eventregister_pending;
+    sendMessage(QString("clientnotifyregister schandlerid=%1 event=notifytalkstatuschange \n"
+
+                        "clientnotifyregister schandlerid=%1 event=notifyclientleftview \n"
+                        "clientnotifyregister schandlerid=%1 event=notifycliententerview \n"
+                        "clientnotifyregister schandlerid=%1 event=notifyclientupdated \n"
+                        "clientnotifyregister schandlerid=%1 event=notifyclientmoved \n"
+
+                        "clientnotifyregister schandlerid=%1 event=notifychannelcreated \n"
+                        "clientnotifyregister schandlerid=%1 event=notifychanneledited \n"
+                        "clientnotifyregister schandlerid=%1 event=notifychanneldeleted \n"
+                        "clientnotifyregister schandlerid=%1 event=notifychannelmoved \n"
+                        ).arg(0));
 }
 
 void LH_QtPlugin_TS3::TS3Disconnected()
 {
-    qDebug() << "LH_TS3: Disconnected";
+    //qDebug() << "LH_TS3: Disconnected";
+    updateStatus(false);
+    server_action_ = sa_disconnected;
     if(server_action_ == sa_reconnect)
-    {
-        server_action_ = sa_none;
         openConnection();
+    else
+        tryConnectTimer_.restart();
+}
+
+void LH_QtPlugin_TS3::TS3ConnectionError(QAbstractSocket::SocketError socketError)
+{
+    if(socketError == QAbstractSocket::ConnectionRefusedError)
+    {
+        server_action_ = sa_disconnected;
+        tryConnectTimer_.restart();
+        //qDebug() << "LH_TS3: Connection Refused - will try again in a few moments";
     }
+    else
+    {
+        qWarning() << "LH_TS3: Connection Error - " << socket_->errorString();
+        server_action_ = sa_disconnected;
+        tryConnectTimer_.restart();
+    }
+}
+
+responseResult LH_QtPlugin_TS3::parseResult(QString msg)
+{
+    QRegExp rx("error id=(\\S+) msg=(\\S+)(?: extra_msg=(\\S+)){0,1}");
+    if(rx.indexIn(msg)==-1)
+        return (responseResult){false, false, 0, "", ""};
+    else
+        return (responseResult){true, (rx.cap(1)=="0"), (rx.cap(1).toInt()), rx.cap(2).replace("\\s"," "), rx.cap(3).replace("\\s"," ")};
 }
 
 void LH_QtPlugin_TS3::TS3DataReceived()
 {
     QString receivedMsg = QString(socket_->readAll());
-    QRegExp rxResult("error id=(\\S+) msg=(\\S+)(?: extra_msg=(\\S+)){0,1}");
-    QRegExp rxSCHandler("selected schandlerid=([0-9]*)");
-    QRegExp rxNoteTalkChange("notifytalkstatuschange schandlerid=([0-9]*) status=([0-9]*) isreceivedwhisper=([0-9]*) clid=([0-9]*)");
-    responseResult result = (rxResult.indexIn(receivedMsg)==-1? (responseResult){false, false, 0, "", ""} : (responseResult){true, (rxResult.cap(1)=="0"), (rxResult.cap(1).toInt()), rxResult.cap(2).replace("\\s"," "), rxResult.cap(3).replace("\\s"," ")});
+    //QRegExp rxSCHandler("selected schandlerid=([0-9]*)");
+    QRegExp rxNotify("notify(\\w*) schandlerid=([0-9]*) (.*)");
+    responseResult result = parseResult(receivedMsg);
 
     if(result.isResult && !result.isValid)
-        qWarning() << QString("LH_TS3: Error: %1%2").arg(result.errorMessage, (result.extraMessage==""?"":QString(" (%1)").arg(result.extraMessage)));
-    else if(rxSCHandler.indexIn(receivedMsg)!=-1)
-        schandlerid_ = rxSCHandler.cap(1).toInt();
-    else if(rxNoteTalkChange.indexIn(receivedMsg)!=-1)
-        talkChanged((rxNoteTalkChange.cap(2).toInt()!=0),(rxNoteTalkChange.cap(3).toInt()!=0),rxNoteTalkChange.cap(4).toInt());
+    {
+        if(result.errorCode == 1794)
+        {
+            channels_.clear();
+            clients_.clear();
+            speakers_.clear();
+            refreshSpeakers();
+            //qDebug() << "LH_TS3: Client is not connected to a server.";
+            updateStatus(true);
+        }
+        else
+        {
+            qWarning() << QString("LH_TS3: Unhandled Response Error: %1%2 (%3)").arg(result.errorMessage, (result.extraMessage==""?"":QString(" (%1)").arg(result.extraMessage))).arg(result.errorCode);
+            Q_ASSERT(false);
+        }
+    }
+    else if(rxNotify.indexIn(receivedMsg)!=-1)
+    {
+        if(rxNotify.cap(1)=="talkstatuschange")
+            talkChanged(rxNotify.cap(3));
+        if(QString("clientleftview,cliententerview,clientupdated,clientmoved").split(',').contains(rxNotify.cap(1)) ||
+           QString("channelcreated,channeledited,channeldeleted,channelmoved").split(',').contains(rxNotify.cap(1)) )
+        {
+            server_action_ = sa_channellist_pending;
+            sendMessage("channellist");
+        }
+    }
     else switch(server_action_)
     {
+    case sa_eventregister_pending:
+        if(!result.isResult)
+            break;
+        server_action_ = sa_channellist_pending;
+        sendMessage("channellist");
+        break;
     case sa_channellist_pending:
         if(channels_.load(receivedMsg))
         {
+            updateStatus(true,true,true);
             //qDebug()<<"LH_TS3: Server has " << channels_.length() << " channel(s)";
             server_action_ = sa_clientlist_pending;
             sendMessage("clientlist -voice");
@@ -136,11 +213,13 @@ void LH_QtPlugin_TS3::TS3DataReceived()
     case sa_clientlist_pending:
         if(clients_.load(receivedMsg))
         {
+            updateStatus(true,true,true,true);
             server_action_ = sa_listening;
-            sendMessage(QString("clientnotifyregister schandlerid=%1 event=notifytalkstatuschange").arg(schandlerid_));
         }
         break;
     case sa_none:
+    case sa_disconnected:
+    case sa_connecting:
     case sa_reconnect:
     case sa_listening:
         break;
@@ -153,9 +232,21 @@ int LH_QtPlugin_TS3::sendMessage(QString msg)
     return socket_->write(QString("%1\n").arg(msg).toUtf8());
 }
 
-void LH_QtPlugin_TS3::talkChanged(bool status, bool whisper, int clid)
+void LH_QtPlugin_TS3::talkChanged(QString params)
 {
-    Q_UNUSED(whisper);
+    QRegExp rx("status=([0-9]*) isreceivedwhisper=([0-9]*) clid=([0-9]*)");
+
+    bool status = false;
+    bool whisper = false;
+    int clid = -1;
+
+    if(rx.indexIn(params)!=-1)
+    {
+        status = (rx.cap(1).toInt()!=0);
+        whisper = (rx.cap(2).toInt()!=0);
+        clid = rx.cap(3).toInt();
+    }
+
     if(status)
     {
         if(speakers_.contains(clid))
@@ -166,9 +257,27 @@ void LH_QtPlugin_TS3::talkChanged(bool status, bool whisper, int clid)
             return;
         speakers_.remove(clid);
     }
-    QString newSpeakers = speakers_.toString();
-    //qDebug() << "Speakers: " << newSpeakers;
-    setup_speakers->setValue(newSpeakers);
-    emit speakersChanged(newSpeakers);
+    refreshSpeakers();
 }
 
+void LH_QtPlugin_TS3::refreshSpeakers()
+{
+    QString speakers = speakers_.toString();
+    if(setup_speakers->value()!=speakers)
+    {
+        //qDebug() << "Speakers: " << speakers;
+        setup_speakers->setValue(speakers);
+        emit speakersChanged(speakers);
+    }
+}
+
+void LH_QtPlugin_TS3::updateStatus(bool isRunning, bool isConnected, bool showChannels, bool showClients)
+{
+    setup_status->setHelp(QString("<table><tr><td><img src=':/images/%5.png'/></td><td>TS3 is %1%2.</td></tr></table><hr/>Channels: %3<br/>Users: %4<hr/>")
+            .arg(isRunning? "running" : "not running")
+            .arg(isRunning? (isConnected? " and is connected" : " but is not connected") : "")
+            .arg(showChannels? QString::number(channels_.count()) : "N/A")
+            .arg(showClients? QString::number(clients_.count()) : "N/A")
+            .arg(!isRunning? "notrunning" : (isConnected? "active" : "unconnected"))
+            );
+}

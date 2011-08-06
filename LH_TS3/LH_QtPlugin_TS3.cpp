619f2987716fa5cf3036db3db77f5cca2e68b5d8
Andy Bridges <andy@bridgesuk.com>
2011-08-06 18:54:37 +0100

    LH_TS3: Updated the source link settings

--- LH_TS3/LH_QtPlugin_TS3.cpp
+++ LH_TS3/LH_QtPlugin_TS3.cpp
@@ -74,33 +74,27 @@ const char *LH_QtPlugin_TS3::userInit()
     setup_user_detail_->setHelp("");
 
     setup_nickname_ = new LH_Qt_QString(this, "Nickname", "", /*LH_FLAG_HIDDEN | LH_FLAG_READONLY |*/ LH_FLAG_NOSAVE | LH_FLAG_NOSINK);
-    setup_nickname_->item()->states |= LH_STATE_SOURCE;
-    setup_nickname_->setLink("Monitoring/3rdParty/TeamSpeak3/Nickname");
+    setup_nickname_->setLink("Monitoring/3rdParty/TeamSpeak3/Nickname", true);
     setup_nickname_->refreshData();
 
     setup_talking_ = new LH_Qt_QString(this, "Talking", "", /*LH_FLAG_HIDDEN | LH_FLAG_READONLY |*/ LH_FLAG_NOSAVE | LH_FLAG_NOSINK);
-    setup_talking_->item()->states |= LH_STATE_SOURCE;
-    setup_talking_->setLink("Monitoring/3rdParty/TeamSpeak3/Talking");
+    setup_talking_->setLink("Monitoring/3rdParty/TeamSpeak3/Talking", true);
     setup_talking_->refreshData();
 
     setup_channelname_ = new LH_Qt_QString(this, "Channel", "", /*LH_FLAG_HIDDEN | LH_FLAG_READONLY |*/ LH_FLAG_NOSAVE | LH_FLAG_NOSINK);
-    setup_channelname_->item()->states |= LH_STATE_SOURCE;
-    setup_channelname_->setLink("Monitoring/3rdParty/TeamSpeak3/Channel Name");
+    setup_channelname_->setLink("Monitoring/3rdParty/TeamSpeak3/Channel Name",true);
     setup_channelname_->refreshData();
 
     setup_connection_status_ = new LH_Qt_QStringList(this, "Connection Status", QStringList() << "Not Running" << "Not Connected" << "Connected", /*LH_FLAG_HIDDEN | LH_FLAG_READONLY |*/ LH_FLAG_NOSAVE | LH_FLAG_NOSINK );
-    setup_connection_status_->item()->states |= LH_STATE_SOURCE;
-    setup_connection_status_->setLink("Monitoring/3rdParty/TeamSpeak3/Connection Status");
+    setup_connection_status_->setLink("Monitoring/3rdParty/TeamSpeak3/Connection Status",true);
     setup_connection_status_->refreshData();
 
     setup_microphone_status_ = new LH_Qt_QStringList(this, "Microphone Status", QStringList() << "N/A" << "None" << "Muted" << "Active", /*LH_FLAG_HIDDEN | LH_FLAG_READONLY |*/ LH_FLAG_NOSAVE | LH_FLAG_NOSINK );
-    setup_microphone_status_->item()->states |= LH_STATE_SOURCE;
-    setup_microphone_status_->setLink("Monitoring/3rdParty/TeamSpeak3/Microphone Status");
+    setup_microphone_status_->setLink("Monitoring/3rdParty/TeamSpeak3/Microphone Status",true);
     setup_microphone_status_->refreshData();
 
     setup_speakers_status_   = new LH_Qt_QStringList(this, "Speaker Status"   , QStringList() << "N/A" << "None" << "Muted" << "Active", /*LH_FLAG_HIDDEN | LH_FLAG_READONLY |*/ LH_FLAG_NOSAVE | LH_FLAG_NOSINK );
-    setup_speakers_status_->item()->states |= LH_STATE_SOURCE;
-    setup_speakers_status_->setLink("Monitoring/3rdParty/TeamSpeak3/Speaker Status");
+    setup_speakers_status_->setLink("Monitoring/3rdParty/TeamSpeak3/Speaker Status",true);
     setup_speakers_status_->refreshData();
 
     /*

e215a1d77f39030dd2ad3b6ef39e41d902a0962d
Andy Bridges <andy@bridgesuk.com>
2011-08-13 16:47:56 +0100

    array UIs: moved code into cpp files

--- LH_Qt_array_int_ui.h
+++ LH_Qt_array_int_ui.h
@@ -17,29 +17,7 @@ class LH_Qt_array_int_ui : public LH_Qt_array_int
 
     bool fixedRange_;
 
-    void init(lh_setup_type ui_type, int uiFlags, bool _fixedRange = false, qint64 min = 0, qint64 max = 0 )
-    {
-        ui_type_ = ui_type;
-        fixedRange_ = _fixedRange;
-        ui_ = NULL;
-        QString uiIdent = QString("%1__ui__").arg(ident());
-        if(ui_type == lh_type_integer && !fixedRange_)
-            ui_ = new LH_Qt_int(parent(), uiIdent.toUtf8(), 0, uiFlags );
-        if(ui_type == lh_type_integer && fixedRange_)
-            ui_ = new LH_Qt_int(parent(), uiIdent.toUtf8(), 0, min, max, uiFlags );
-        if(ui_type == lh_type_integer_color)
-            ui_ = new LH_Qt_QColor(parent(), uiIdent.toUtf8(), QColor::fromRgba(0), uiFlags );
-
-        Q_ASSERT(ui_ != NULL);
-
-        ui_->setTitle(ident());
-        uiIndex_ = 0;
-        updatingUI_ = false;
-
-        connect(this, SIGNAL(changed()), this, SLOT(arrayValuesChanged()));
-        connect(this, SIGNAL(set()), this, SLOT(arrayValuesChanged()));
-        connect(ui_, SIGNAL(changed()), this, SLOT(uiValueChanged()));
-    }
+    void init(lh_setup_type ui_type, int uiFlags, bool _fixedRange = false, qint64 min = 0, qint64 max = 0 );
 
 public:
     LH_Qt_array_int_ui( LH_QtObject *parent, const char *ident, int size = 0, int flags = 0, lh_setup_type ui_type = lh_type_integer)
@@ -54,62 +32,20 @@ public:
         init(ui_type, LH_FLAG_NOSAVE | LH_FLAG_NOSINK | LH_FLAG_NOSOURCE | flags, true, min, max);
     }
 
-    void setEditIndex(int index) {
-        uiIndex_ = index;
-        arrayValuesChanged();
-    }
+    void setEditIndex(int index);
 
-    void setFlag(int f, bool state)
-    {
-        if(f != LH_FLAG_HIDDEN)
-            LH_Qt_array_int::setFlag(f, state);
-        ui_->setFlag(f, state);
-    }
+    void setFlag(int f, bool state);
 
-    virtual void setHelp(const QString& s)
-    {
-        LH_Qt_array_int::setHelp(s);
-        ui_->setHelp(s);
-    }
+    virtual void setHelp(const QString& s);
 
-    void setTitle(const char *s = 0)
-    {
-        LH_Qt_array_int::setTitle(s);
-        ui_->setTitle(s);
-    }
+    void setTitle(const char *s = 0);
 
-    void setTitle( const QString &s )
-    {
-        LH_Qt_array_int::setTitle(s);
-        ui_->setTitle(s);
-    }
+    void setTitle( const QString &s );
 
 public slots:
-    void arrayValuesChanged() {
-        if (uiIndex_>=0 && uiIndex_ < this->size())
-        {
-            updatingUI_ = true;
-            if (ui_type_ == lh_type_integer && !fixedRange_)
-                ((LH_Qt_int*)ui_)->setMinMax(this->min(),this->max());
-            if(ui_type_ == lh_type_integer)
-                ;//((LH_Qt_int*)ui_)->setValue((this->at(uiIndex_))); // causes assert failure!
-            if(ui_type_ == lh_type_integer_color)
-                ((LH_Qt_QColor*)ui_)->setValue(QColor::fromRgba(this->at(uiIndex_)));
-            updatingUI_ = false;
-        }
-    }
+    void arrayValuesChanged();
 
-    void uiValueChanged(){
-        if (updatingUI_) return;
-        if (uiIndex_>=0 && uiIndex_ < this->size())
-        {
-            if(ui_type_ == lh_type_integer)
-                this->setAt(uiIndex_, ((LH_Qt_int*)ui_)->value());
-            if(ui_type_ == lh_type_integer_color)
-                this->setAt(uiIndex_, ((LH_Qt_QColor*)ui_)->value().rgba());
-
-        }
-    }
+    void uiValueChanged();
 
 };
 

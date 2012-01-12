918f05f3f318d1d24e9cae5bf1a2d4f46d1461c1
Andy Bridges <andy@bridgesuk.com>
2012-01-12 22:54:03 +0000

    LH_DataViewer: assorted tests

--- codeleap/DataViewerTestApp/mainwindow.cpp
+++ codeleap/DataViewerTestApp/mainwindow.cpp
@@ -11,6 +11,8 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->drinkButton, SIGNAL(clicked()), this, SLOT(drinkSelectedPotion()));
     connect(ui->castButton, SIGNAL(clicked()), this, SLOT(castSpell()));
     connect(ui->damageButton, SIGNAL(clicked()), this, SLOT(damagePlayer()));
+    connect(ui->changeName, SIGNAL(clicked()), this, SLOT(changeName()));
+    connect(ui->changeRace, SIGNAL(clicked()), this, SLOT(changeRace()));
 }
 
 MainWindow::~MainWindow()
@@ -38,6 +40,16 @@ void MainWindow::init()
 
     for(int i = 0; i<potions.count(); i++)
         ui->potionsList->addItem(QString("%1 (%2gp)").arg(potions.at(i).name).arg(potions.at(i).price));
+
+    ui->racesList->addItem("Human");
+    ui->racesList->addItem("Elf");
+    ui->racesList->addItem("Dwarf");
+    ui->racesList->addItem("Orc");
+    ui->racesList->addItem("Half-Elf");
+    ui->racesList->addItem("Half-Orc");
+    ui->racesList->addItem("Gnome");
+    ui->racesList->addItem("Troll");
+    ui->racesList->addItem("Dragonkin");
 }
 
 void MainWindow::generateCharacter(character *c)
@@ -45,7 +57,8 @@ void MainWindow::generateCharacter(character *c)
     QTime now = QTime::currentTime();
     srand((uint)now.msec());
 
-    c->name = "Player";
+    c->setName("Player");
+    c->setRace("Human");
     c->gold = 100;
 
     c->dexterity.base = qrand() % 20;
@@ -64,7 +77,8 @@ void MainWindow::refreshSheet(character *c)
 {
     ui->textEdit->clear();
     QString str = "";
-    str += QString("<b>Name:</b> %1<hr />").arg(c->name);
+    str += QString("<b>Name:</b> %1<hr />").arg(c->name());
+    str += QString("<b>Race:</b> %1<hr />").arg(c->race());
     str += QString("<b>Strength:</b> %1 (%2)<br />").arg(c->strength.current).arg(c->strength.base);
     str += QString("<b>Dexterity:</b> %1 (%2)<br />").arg(c->dexterity.current).arg(c->dexterity.base);
     str += QString("<b>Intelligence:</b> %1 (%2)<br />").arg(c->intelligence.current).arg(c->intelligence.base);
@@ -97,5 +111,18 @@ void MainWindow::damagePlayer()
     player_->health.current -= (qrand() % 100)/10.0;
     if(player_->health.current < 0)
         player_->health.current = 0;
+    refreshSheet(player_);
+}
+
+void MainWindow::changeName()
+{
+    player_->setName(ui->lineEdit->text());
+    ui->lineEdit->setText("");
+    refreshSheet(player_);
+}
+
+void MainWindow::changeRace()
+{
+    player_->setRace(ui->racesList->currentText());
     refreshSheet(player_);
 }

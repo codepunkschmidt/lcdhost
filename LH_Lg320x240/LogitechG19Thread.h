812930c4fdb6e34ceaad86f8bb3ceb678145eae0
Johan Lindh <johan@linkdata.se>
2012-05-13 20:32:24 +0200

    cleaning win32 g19-usb

--- LH_Lg320x240/LogitechG19Thread.h
+++ LH_Lg320x240/LogitechG19Thread.h
@@ -2,7 +2,6 @@
 #define LOGITECHG19THREAD_H
 
 #include <QThread>
-#include <QSemaphore>
 #include <QImage>
 
 #include "LogitechG19.h"
@@ -11,19 +10,14 @@ class LogitechG19Thread : public QThread
 {
     Q_OBJECT
 
-    LogitechG19 * g19_;
     volatile bool time_to_die_;
-    QSemaphore sem_;
-    QImage img_;
 
 public:
     explicit LogitechG19Thread( QObject *parent = 0 );
 
     void run();
 
-    LogitechG19 *device() const { return g19_; }
     void timeToDie() { time_to_die_ = true; }
-    bool gotDevice() const { return g19_ != NULL; }
     bool waitToDie(int secs);
 };
 

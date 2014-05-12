61d19089686b62279db655d234e4f599a97fc8e1
Johan Lindh <johan@linkdata.se>
2014-05-12 09:27:43 +0200

    added network total received/sent

--- plugins/LH_Text/LH_TextNetInbound.cpp
+++ plugins/LH_Text/LH_TextNetInbound.cpp
@@ -1,7 +1,7 @@
 /**
   \file     LH_TextNetInbound.cpp
   \author   Johan Lindh <johan@linkdata.se>
-  \legalese Copyright (c) 2009-2010 Johan Lindh
+  \legalese Copyright (c) 2009-2014 Johan Lindh
 
   All rights reserved.
 
